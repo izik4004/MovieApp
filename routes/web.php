296b058a5591c4421cf3d97675a75a8afa24608d
unknown <izik4004>
2021-07-03 23:16:13 +0100

    fetched api

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', 'App\Http\Controllers\MoviescOntroller@index');
+Route::get('/movies/{movie}', 'MoviesController@show');
 
-Route::get('show', function () {
-    return view('show');
-});
 
 
